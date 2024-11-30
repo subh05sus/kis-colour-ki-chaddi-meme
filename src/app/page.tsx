db8eeb05ed0ca0b022350401009d53eb8fa04bce
subh05sus <sahasubhadipfkk@gmail.com>
2024-11-30 12:25:36 +0530

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,100 +1,101 @@
+"use client";
+import GameIconsUnderwear from "@/components/chaddi";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Copy } from "lucide-react";
 import Image from "next/image";
+import React, { useEffect, useState } from "react";
+import { IoLogoWhatsapp } from "react-icons/io";
 
 export default function Home() {
+  const [color, setColor] = useState("white");
+  const [chaddiColour, setChaddiColour] = useState("white");
+  const [chaddiText, setChaddiText] = useState(false);
+
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const colorParam = params.get("color");
+    if (colorParam) {
+      const decodedColor = decodeURIComponent(colorParam);
+      setColor(decodedColor);
+      setChaddiColour(decodedColor);
+      setChaddiText(true);
+    }
+  }, []);
+
+  const handleButtonClick = () => {
+    setChaddiColour(color);
+    const audio = new Audio("/music.mp3");
+    audio.play();
+    setChaddiText(true);
+  };
+
+  const shareButtonClick = () => {
+    const url = new URL(window.location.href);
+    url.searchParams.set("color", encodeURIComponent(color));
+    navigator.clipboard.writeText(url.toString());
+  };
+
+  const whatsappbuttonClick = () => {
+    const url = new URL(window.location.href);
+    url.searchParams.set("color", encodeURIComponent(color));
+    window.open(`https://wa.me/?text=${encodeURIComponent(url.toString())}`, '_blank');
+  };
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+      <main className="flex flex-col gap-8 row-start-2 items-center">
+        <h1 className="text-3xl font-semibold text-neutral-300">
+          Kis Colour ki <span className="font-bold text-white">chaddi</span>{" "}
+          peheno ho ?
+        </h1>
         <Image
-          className="dark:invert"
-          src="/next.svg"
+          className="rounded-xl"
+          src="/chaddi.png"
           alt="Next.js logo"
-          width={180}
-          height={38}
+          width={150}
+          height={300}
           priority
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+        <GameIconsUnderwear color={chaddiColour} fontSize={150} />
+        <Input
+          value={color}
+          onChange={(e) => setColor(e.target.value)}
+          className="rounded-xl text-xl mx-2 max-w-lg"
+          placeholder="Enter Chaddi Colour here"
+        />
+        <div className="flex gap-2">
+          <Button
+            onClick={handleButtonClick}
+            className="rounded-xl px-4 py-2 text-xl"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            Change Chaddi Colour
+          </Button>
+          <Button
+            onClick={shareButtonClick}
+            variant={"secondary"}
+            className="rounded-xl px-4 py-2 text-xl"
           >
-            Read our docs
-          </a>
+            <Copy />
+          </Button>
+          <Button
+            onClick={whatsappbuttonClick}
+            variant={"secondary"}
+            className="rounded-xl px-4 py-2 text-xl"
+          >
+            <IoLogoWhatsapp />
+          </Button>
         </div>
+        {chaddiText && (
+          <p className="text-white">
+            Aaj Maine{" "}
+            <span style={{ color: chaddiColour }}>{chaddiColour}</span> ki
+            peheni h!
+          </p>
+        )}
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+        {`Made With Love and Lust 🫦`}
       </footer>
     </div>
   );
